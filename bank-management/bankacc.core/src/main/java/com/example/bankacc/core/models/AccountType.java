975c4ef340b40f14d5196b23f633f23667b9870d
Rachid Benkirane <rachid.benkirane@gmail.com>
2022-05-01 18:49:11 +0200

    Bank Account Domain Models

---
+++ bank-management/bankacc.core/src/main/java/com/example/bankacc/core/models/AccountType.java
@@ -0,0 +1,5 @@
+package com.example.bankacc.core.models;
+
+public enum AccountType {
+    SAVINGS, CURRENT
+}
--- bank-management/bankacc.query.api/src/main/java/com/example/bankacc/query/api/BankAccQueryApplication.java
+++ bank-management/bankacc.query.api/src/main/java/com/example/bankacc/query/api/BankAccQueryApplication.java
@@ -3,6 +3,7 @@ package com.example.bankacc.query.api;
 import com.example.bankacc.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -10,6 +11,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @SpringBootApplication
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EntityScan(basePackages = "com.example.bankacc.core.models")
 @Import(AxonConfig.class)
 public class BankAccQueryApplication {
 

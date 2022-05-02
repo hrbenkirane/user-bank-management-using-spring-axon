96835028ef5560a36985c4d4f2d3b658e4272bca
Rachid Benkirane <rachid.benkirane@gmail.com>
2022-05-02 21:27:29 +0200

    Implementing Closing BankAccount Controller

--- bank-management/bankacc.cmd.api/src/main/java/com/example/bankacc/cmd/api/controllers/CloseAccountController.java
+++ bank-management/bankacc.cmd.api/src/main/java/com/example/bankacc/cmd/api/controllers/CloseAccountController.java
@@ -1,5 +1,6 @@
 package com.example.bankacc.cmd.api.controllers;
 
+import com.example.bankacc.cmd.api.commands.CloseAccountCommand;
 import com.example.bankacc.cmd.api.commands.DepositFundsCommand;
 import com.example.bankacc.core.dto.BaseResponse;
 import org.axonframework.commandhandling.gateway.CommandGateway;
@@ -10,31 +11,31 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.UUID;
 
 @RestController
-@RequestMapping(path = "/api/v1/depositFunds")
-public class DepositFundsController {
+@RequestMapping(path = "/api/v1/closeBankAccount")
+public class CloseAccountController {
+
     private CommandGateway commandGateway;
 
     @Autowired
-    public DepositFundsController(CommandGateway commandGateway) {
+    public CloseAccountController(CommandGateway commandGateway) {
         this.commandGateway = commandGateway;
     }
 
-    @PutMapping(path = "/{id}")
+    @DeleteMapping(path = "/{id}")
     @PreAuthorize("hasAuthority('WRITE_PRIVILEGE')")
-    public ResponseEntity<BaseResponse> depositFunds(@PathVariable(value = "id") String id,
-                                                     @Valid @RequestBody DepositFundsCommand command) {
-        try{
-            command.setId(id);
+    public ResponseEntity<BaseResponse> closeBankAccount(@PathVariable(value = "id") String id) {
+        try {
+            CloseAccountCommand command = CloseAccountCommand.builder().id(id).build();
             commandGateway.sendAndWait(command);
-            return new ResponseEntity<>(new BaseResponse("successfully deposit funds into bank account"), HttpStatus.OK);
-        } catch (Exception e) {
-            String safeErrorMessage = "Error while processing request to deposit funds into bank account for id - " + id;
+            return new ResponseEntity<>(new BaseResponse("Bank account successfully closed!"), HttpStatus.OK);
+        } catch (Exception e ) {
+            String safeErrorMessage = "Error while processing request to close bank account for id - " + id;
             System.out.println(e.toString());
             return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-    }
-}
 
+    }
+
+}
